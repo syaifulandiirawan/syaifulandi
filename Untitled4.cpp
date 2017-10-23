66cc861daef2ec60d8a0bf1cfaf7cf8484846873
syaifulandi <32381985+syaifulandiirawan@users.noreply.github.com>
2017-10-23 13:30:39 +0700

    menambahkan 4 variabel dengan tipe data double

--- Untitled4.cpp
+++ Untitled4.cpp
@@ -8,4 +8,5 @@ int main(){
 	double luas1Box=lantaiP*8/1000000;
 	cout<<"luas rumah       :"<<luasRumah<<" m2"<<endl;
 	cout<<"luas 1 box parket:"<<luas1Box<<" m2"<<endl;
+	double jumlahBoxP,biayaPG,totalB;
 }
